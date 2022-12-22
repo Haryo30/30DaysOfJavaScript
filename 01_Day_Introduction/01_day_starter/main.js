fd35e2701dfe42d5c1c66b525a4c7754b066a36e
Haryo30 <hrydwntr22@gmail.com>
2022-12-22 14:10:19 +0700

    01_day_Introduction

---
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -0,0 +1,4 @@
+// the variable values can be accessed from other variable.js file
+console.log(firstName, lastName, country);
+console.log(weight, height, ipk);
+console.log(name, job, live);
---
+++ 01_Day_Introduction/01_day_starter/variable.js
@@ -0,0 +1,19 @@
+// Declaring different variables of different data types
+let firstName = 'Haryo'
+let lastName= 'Dewantoro'
+let country = 'Indonesia'
+let city = 'Bekasi'
+let age = 27
+let isMarried = false
+
+// Declaring variables with number values
+const weight = 85
+const height = 170
+const ipk = 3.05
+
+// variables can also be declaring in one line separated by comma
+let name = 'Haryo D',
+    job = 'Programming',
+    live = 'Wahana Babelan'
+
+// console.log()
